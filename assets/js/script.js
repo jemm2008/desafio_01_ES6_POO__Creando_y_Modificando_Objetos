d5851e73c727156f460c65981de5fd67e4beae3d
jemm2008 <jemm2008@gmail.com>
2022-06-02 14:05:45 -0400

    Se sustituyó 'var' por 'let' en las declaraciones de las variables para hacer el código 100% ES5.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,31 @@ abrir 'Inspecionar', luego abrir la "Consola",
 los ejercicios se muestran en la misma.`)
 //
   //  *******  CONSULTORIO  *******  //
-function Consultorio (nombre, pacientes){
-    let _nombre = nombre
-    let _pacientes = pacientes || []
+function Consultorio (nombreC, pacientesC){
+    var _nombreConsultorio = nombreC
+    var _pacientesConsultorio = pacientesC || []
 
     Object.defineProperty(this, "_getNombre", {
         get:function(){
-            return _nombre
+            return _nombreConsultorio
         }
     })
 
     Object.defineProperty(this, "_setNombre", {
-        set:function(newName){
-            return _nombre = newName
+        set:function(newNameC){
+            return _nombreConsultorio = newNameC
         }
     })
 
     Object.defineProperty(this, "_getPacientes", {
         get:function(){
-            return _pacientes
+            return _pacientesConsultorio
         }
     })
 
     Object.defineProperty(this, "_addPaciente", {
-        set:function(newPatient){
-            return _pacientes.push(newPatient)
+        set:function(newPatientC){
+            return _pacientesConsultorio.push(newPatientC)
         }
     })
 }
@@ -36,87 +36,87 @@ function Consultorio (nombre, pacientes){
 Consultorio.prototype.getNombre = function(){
     return this._getNombre
 }
-Consultorio.prototype.setNombre = function(new_name){
-    return this._setNombre = new_name
+Consultorio.prototype.setNombre = function(newname_C){
+    return this._setNombre = newname_C
 }
 
 Consultorio.prototype.getPacientes = function(){
     return this._getPacientes
 }
-Consultorio.prototype.addPaciente = function(paciente){
-    return this._addPaciente = paciente
+Consultorio.prototype.addPaciente = function(newPaciente_C){
+    return this._addPaciente = newPaciente_C
 }
 //  Consultorio. Método Público para "FILTRAR POR EL NOMBRE" y mostrar los datos de un Paciente (Req. 4-a)  //
 Consultorio.prototype.filtrarNombrePaciente = function(nombreA_Filtrar){
-    this.getPacientes().filter(function(paciente) {
-        if (paciente.getNombre() == nombreA_Filtrar){
+    this.getPacientes().filter(function(paciente_X) {
+        if (paciente_X.getNombre() == nombreA_Filtrar){
             return console.log(`
 Datos del paciente:
-                    Nombre:       ${paciente.getNombre()}, 
-                    Edad:         ${paciente.getEdad()},
-                    RUT:          ${paciente.getRut()},
-                    Diagnóstico:  ${paciente.getDiagnOstico()}.
+                    Nombre:       ${paciente_X.getNombre()}, 
+                    Edad:         ${paciente_X.getEdad()},
+                    RUT:          ${paciente_X.getRut()},
+                    Diagnóstico:  ${paciente_X.getDiagnOstico()}.
                     `)
         }
     })
 }
 //  Consultorio. Método Público para mostrar TODOS los Datos de los Pacientes (Req. 4-b)  //
 Consultorio.prototype.mostrarFullData = function(){
-    return this.getPacientes().forEach(function(paciente, id) {
-        console.log(`paciente ${id+1} Nombre: ${paciente.getNombre()} - Rut: ${paciente.getRut()} - Edad: ${paciente.getEdad()} - Diagnóstico: ${paciente.getDiagnOstico()}`)
+    return this.getPacientes().forEach(function(paciente_All, id) {
+        console.log(`paciente_All ${id+1} Nombre: ${paciente_All.getNombre()} - Rut: ${paciente_All.getRut()} - Edad: ${paciente_All.getEdad()} - Diagnóstico: ${paciente_All.getDiagnOstico()}`)
     })
 }
   //  *******  FIN  CONSULTORIO  *******  //
 //
 //
   //  *******  PACIENTE  *******  //
-function Paciente (nombre, edad, rut, diagnOstico){
-    let _nombre = nombre
-    let _edad = edad
-    let _rut = rut
-    let _diagnOstico = diagnOstico || ""
+function Paciente (nombreP, edadP, rutP, diagnOsticoP){
+    var _nombrePaciente = nombreP
+    var _edadPaciente = edadP
+    var _rutPaciente = rutP
+    var _diagnOsticoPaciente = diagnOsticoP || ""
 
     Object.defineProperty(this, "_getNombre", {
         get:function(){
-            return _nombre
+            return _nombrePaciente
         }
     })    
     Object.defineProperty(this, "_setNombre", {
-        set:function(newName){
-            return _nombre = newName
+        set:function(newName_P){
+            return _nombrePaciente = newName_P
         }
     })
 
     Object.defineProperty(this, "_getEdad", {
         get:function(){
-            return _edad
+            return _edadPaciente
         }
     })   
     Object.defineProperty(this, "_setEdad", {
-        set:function(newAge){
-            return _edad = newAge
+        set:function(newAge_P){
+            return _edadPaciente = newAge_P
         }
     })    
 
     Object.defineProperty(this, "_getRut", {
         get:function(){
-            return _rut
+            return _rutPaciente
         }
     })   
     Object.defineProperty(this, "_setRut", {
-        set:function(newRut){
-            return _rut = newRut
+        set:function(newRut_P){
+            return _rutPaciente = newRut_P
         }
     })   
 
     Object.defineProperty(this, "_getDiagnOstico", {
         get:function(){
-            return _diagnOstico
+            return _diagnOsticoPaciente
         }
     })
     Object.defineProperty(this, "_setDiagnOstico", {
-        set:function(newDiagnostic){
-            return _diagnOstico = newDiagnostic
+        set:function(newDiagnostic_P){
+            return _diagnOsticoPaciente = newDiagnostic_P
         }
     })
 }
@@ -124,29 +124,29 @@ function Paciente (nombre, edad, rut, diagnOstico){
 Paciente.prototype.getNombre = function(){
     return this._getNombre
 }
-Paciente.prototype.setNombre = function(new_name){
-    return this._setNombre = new_name
+Paciente.prototype.setNombre = function(new_name_Pac){
+    return this._setNombre = new_name_Pac
 }
 
 Paciente.prototype.getEdad = function(){
     return this._getEdad
 }
-Paciente.prototype.setEdad = function(new_Age){
-    return this._setEdad = new_Age
+Paciente.prototype.setEdad = function(new_Age_Pac){
+    return this._setEdad = new_Age_Pac
 }
 
 Paciente.prototype.getRut = function(){
     return this._getRut
 }
-Paciente.prototype.setRut = function(new_rut){
-    return this._setRut = new_rut
+Paciente.prototype.setRut = function(new_rut_Pac){
+    return this._setRut = new_rut_Pac
 }
 
 Paciente.prototype.getDiagnOstico = function(){
     return this._getDiagnOstico
 }
-Paciente.prototype.setDiagnOstico = function(new_diagnostic){
-    return this._setDiagnOstico = new_diagnostic
+Paciente.prototype.setDiagnOstico = function(new_diagnostic_Pac){
+    return this._setDiagnOstico = new_diagnostic_Pac
 }
   //  *******  FIN  PACIENTE  *******  //
 //
@@ -154,28 +154,32 @@ Paciente.prototype.setDiagnOstico = function(new_diagnostic){
   //  *******  INICIO  EJECUCION  DE  CODIGO  *******  //
 //
 // Crear Consultorio:
-let clinica = new Consultorio("Jose Gregorio")
+var clinica = new Consultorio("Jose Gregorio")
 console.log("Contenido inicial del Arreglo de Pacientes:")
-console.log(`Array length = ${clinica.getPacientes().length}`)
-console.log(clinica.getPacientes())
+var empty0 = [...clinica.getPacientes()]
+console.log(empty0)
+   //console.log(`Array length = ${clinica.getPacientes().length}`)
+   //console.log(clinica.getPacientes().length)
+   //console.log(clinica.getPacientes())
 //
 // Crear Pacientes:
-let p1 = new Paciente("Juan", 47, "25123653-6")
-let p2 = new Paciente("María", 43, "25123421-6")
-let p3 = new Paciente("Juana", 16, "25123423-2")
-let p4 = new Paciente("José", 14, "25123433-K")
-let p5 = new Paciente("Peter", 30,"19123009-1")
-let p6 = new Paciente("Sherlock", 57,"6123597-4")
-let p7 = new Paciente("Watson", 51,"7123710-K")
+var paX_1 = new Paciente("Juan", 47, "25123653-6")
+var paX_2 = new Paciente("María", 43, "25123421-6")
+var paX_3 = new Paciente("Juana", 16, "25123423-2")
+var paX_4 = new Paciente("José", 14, "25123433-K")
+var paX_5 = new Paciente("Peter", 30,"19123009-1")
+var paX_6 = new Paciente("Sherlock", 57,"6123597-4")
+var paX_7 = new Paciente("Watson", 51,"7123710-K")
 // Añadir pacientes al Consultorio:
 //
-clinica.addPaciente(p1)
-clinica.addPaciente(p2)
-clinica.addPaciente(p3)
-clinica.addPaciente(p4)
-clinica.addPaciente(p5)
-clinica.addPaciente(p6)
-clinica.addPaciente(p7)
+clinica.addPaciente(paX_1)
+clinica.addPaciente(paX_2)
+clinica.addPaciente(paX_3)
+clinica.addPaciente(paX_4)
+clinica.addPaciente(paX_5)
+clinica.addPaciente(paX_6)
+clinica.addPaciente(paX_7)
+// console.log(clinica.getPacientes().length, Array.isArray(clinica.getPacientes()))
     console.log("Contenido final del Arreglo Pacientes:")
 // Método para "mostrar todos los datos" de todos los pacientes // 
     clinica.mostrarFullData()
@@ -197,8 +201,14 @@ clinica.getPacientes()[3].setDiagnOstico("Fotofobia")
 clinica.getPacientes()[4].setDiagnOstico("Alergia")
 clinica.getPacientes()[5].setDiagnOstico("Gripe")
 clinica.getPacientes()[6].setDiagnOstico("Asma")
-    console.log(`Paciente 7 Diagnóstico: ${clinica.getPacientes()[6].getDiagnOstico()}`)
+    console.log(`Paciente 1 Diagnóstico: ${clinica.getPacientes()[0].getDiagnOstico()}`)
+    console.log(`Paciente 2 Diagnóstico: ${clinica.getPacientes()[1].getDiagnOstico()}`)
     console.log(`Paciente 3 Diagnóstico: ${clinica.getPacientes()[2].getDiagnOstico()}`)
+    console.log(`Paciente 4 Diagnóstico: ${clinica.getPacientes()[3].getDiagnOstico()}`)
+    console.log(`Paciente 5 Diagnóstico: ${clinica.getPacientes()[4].getDiagnOstico()}`)
+    console.log(`Paciente 6 Diagnóstico: ${clinica.getPacientes()[5].getDiagnOstico()}`)
+    console.log(`Paciente 7 Diagnóstico: ${clinica.getPacientes()[6].getDiagnOstico()}`)
+
 //
     console.log("*******************************************")
 //
